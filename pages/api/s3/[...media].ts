aa01fb1c4e1f5b73d5526a9dc3f9905a0002dba7
Mark Philip <markphd@pm.me>
2024-07-22 21:00:46 +1000

    initial commit

--- pages/api/s3/[...media].ts
+++ pages/api/s3/[...media].ts
@@ -1,32 +1,38 @@
 import {
   mediaHandlerConfig,
   createMediaHandler,
-} from 'next-tinacms-s3/dist/handlers'
+} from "next-tinacms-s3/dist/handlers";
 
-import { isAuthorized } from '@tinacms/auth'
+import { isAuthorized } from "@tinacms/auth";
 
-export const config = mediaHandlerConfig
+export const config = mediaHandlerConfig;
 
-export default createMediaHandler({
-  config: {
-    credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY || '',
-      secretAccessKey: process.env.S3_SECRET_KEY || '',
+export default createMediaHandler(
+  {
+    config: {
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY || "",
+        secretAccessKey: process.env.S3_SECRET_KEY || "",
+      },
+      endpoint: process.env.S3_ENDPOINT,
+      region: process.env.S3_REGION,
     },
-    region: process.env.S3_REGION,
-  },
-  bucket: process.env.S3_BUCKET || '',
-  authorized: async (req, _res) => {
-    if (process.env.NODE_ENV === 'development') {
-      return true
-    }
-    try {
-      const user = await isAuthorized(req)
+    bucket: process.env.S3_BUCKET || "",
+    authorized: async (req, _res) => {
+      if (process.env.NODE_ENV === "development") {
+        return true;
+      }
+      try {
+        const user = await isAuthorized(req);
 
-      return user && user.verified
-    } catch (e) {
-      console.error(e)
-      return false
-    }
+        return user && user.verified;
+      } catch (e) {
+        console.error(e);
+        return false;
+      }
+    },
   },
-})
+  {
+    cdnUrl: "https://link.storjshare.io/raw/jujjtfmneqrmutttzvxupz655lba/www",
+  }
+);
